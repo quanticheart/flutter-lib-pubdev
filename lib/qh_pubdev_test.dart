5e33b9f5564480b5abeb57b3bf93ddba8eea1fcd
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-01-05 21:41:34 -0300

    update lib

--- lib/qh_pubdev_test.dart
+++ lib/qh_pubdev_test.dart
@@ -1,7 +1,12 @@
 library qh_pubdev_test;
 
- // Exports
+// Exports
+export 'extentions/msg_ext.dart';
+export 'widgets/button_social_signin.dart';
+export 'widgets/view_space.dart';
 
- export 'extentions/msg_ext.dart';
- export 'widgets/button_social_signin.dart';
- export 'widgets/view_space.dart';
+export 'src/custom_check.dart';
+export 'src/custom_switch.dart';
+export 'src/custom_text.dart';
+export 'src/custom_button.dart';
+export 'src/custom_progress.dart';
